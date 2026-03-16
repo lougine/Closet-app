6ef99330ce4218b0bf53c9fa5df7035faeaa66ce
ramyysameh <ramy.sameh2@gmail.com>
2026-03-16 03:58:09 +0200

    nigga

--- src/routes/garmentRoutes.js
+++ src/routes/garmentRoutes.js
@@ -6,10 +6,10 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 router.use(authMiddleware);
 
-router.post("/", garmentController.createGarment);
+router.post("/", garmentController.uploadImage, garmentController.createGarment);
 router.get("/", garmentController.getGarments);
 router.get("/:id", garmentController.getGarmentById);
-router.put("/:id", garmentController.updateGarment);
+router.put("/:id", garmentController.uploadImage, garmentController.updateGarment);
 router.delete("/:id", garmentController.deleteGarment);
 
 module.exports = router;
